f4402f5a9c052e56d4832bfc11cdbf2c50bf446b
Nelson Tran <nelsontran@icloud.com>
2021-09-20 00:12:09 -0700

    add bodyParser

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const app: Express = express();
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 // const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
